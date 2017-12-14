Fix rotation of the sky to work on 3D - fix bugs in sensor lenses orientation - update compound eyes and sensor to tilt correctly - finalise tilting and star creating datasets for tilting

agent/cx.py
@@ -60,7 +60,7 @@ class CXAgent(Agent):
         if super(CXAgent, self).reset():
             # reset to the nest instead of the feeder
             self.pos[:2] = self.nest.copy()
-            self.rot[1] = (self.homing_routes[-1].phi[-2] + np.pi) % (2 * np.pi)
+            self.rot[1] = (self.homing_routes[-1].phi_z[-2] + np.pi) % (2 * np.pi)
             self._net.update = True
 
             return True
@@ -214,7 +214,7 @@ class CXAgent(Agent):
             flow = -get_sph_flow(
                 n_val=np.array(now).flatten(),
                 o_val=np.array(previous).flatten(),
-                rdir=np.array([self.world.eye.theta, self.world.eye.phi]).T,
+                rdir=np.array([self.world.eye.theta_z, self.world.eye.phi_z]).T,
                 rsensor=np.array([[0, self._net.tn_prefs], [0, -self._net.tn_prefs]])
             ).mean(axis=1)  # TODO: scale the value so that it fits better
             flow = self.dx * flow / np.sqrt(np.square(flow).sum())
@@ -269,7 +269,7 @@ class CXLogger(Logger):
         self.hist["flow0"] = []
         self.hist["v0"] = []
         self.hist["v1"] = []
-        self.hist["phi"] = []
+        self.hist["phi_z"] = []
         self.hist["sun"] = []
         self.hist["motor0"] = []
         self.hist["outbound_end"] = -1
@@ -384,7 +384,7 @@ if __name__ == "__main__":
 
             plt.subplot(5, 2, 3)
             plt.grid()
-            plt.plot(x, np.array(agent.log.hist["phi"]), label="phi")
+            plt.plot(x, np.array(agent.log.hist["phi_z"]), label="phi_z")
             plt.plot(x, np.array(agent.log.hist["sun"]), label="rel sun")
             plt.plot(x, np.ones_like(x) * world.sky.lon, label="abs sun")
             plt.legend()
@@ -392,7 +392,7 @@ if __name__ == "__main__":
             plt.xlim(xlim)
             plt.yticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi], ["-pi", "-pi/2", "0", "pi/2", "pi"])
             plt.ylim([-np.pi, np.pi])
-            plt.ylabel("phi")
+            plt.ylabel("phi_z")
 
             plt.subplot(5, 2, 5)
             plt.grid()

agent/temp.py
@@ -59,7 +59,7 @@ class CXAgent(Agent):
         if super(CXAgent, self).reset():
             # reset to the nest instead of the feeder
             self.pos[:2] = self.nest.copy()
-            self.rot[1] = (self.homing_routes[-1].phi[-2] + np.pi) % (2 * np.pi)
+            self.rot[1] = (self.homing_routes[-1].phi_z[-2] + np.pi) % (2 * np.pi)
 
             self.hist["cpu1"] = []
             self.hist["cpu4"] = []
@@ -71,7 +71,7 @@ class CXAgent(Agent):
             self.hist["flow0"] = []
             self.hist["v0"] = []
             self.hist["v1"] = []
-            self.hist["theta"] = []
+            self.hist["theta_z"] = []
             self.hist["motor0"] = []
             self.hist["outbound_end"] = -1
 
@@ -139,7 +139,7 @@ class CXAgent(Agent):
             sun = self.read_sensor()
             v_trans = self.transform_velocity(sun, v.copy())
             flow = self._net.get_flow(sun, v_trans)
-            # flow = self._net.get_flow(__phi, v)
+            # flow = self._net.get_flow(__phi_z, v)
 
             # make a forward pass from the network
             motor = self._net(sun, flow)
@@ -153,7 +153,7 @@ class CXAgent(Agent):
             self.hist["flow0"].append(flow)
             self.hist["v0"].append(v)
             self.hist["v1"].append(v_trans)
-            self.hist["theta"].append(sun)
+            self.hist["theta_z"].append(sun)
             self.hist["tl2"].append(self._net.tl2)
             self.hist["cl1"].append(self._net.cl1)
             self.hist["cpu1"].append(self._net.cpu1)
@@ -225,13 +225,13 @@ class CXAgent(Agent):
             sun = self.read_sensor()
             v_trans = self.transform_velocity(sun, v.copy())
             flow = self._net.get_flow(sun, v_trans)
-            # flow = self._net.get_flow(__phi, v)
+            # flow = self._net.get_flow(__phi_z, v)
 
-            # d_phi = __phi - pphi
+            # d_phi = __phi_z - pphi
             # d_phi = (d_phi + np.pi) % (2 * np.pi) - np.pi
             # make a forward pass from the network
             motor = self._net(sun, flow)
-            # motor = self._net(__phi + np.sign(d_phi) * self.bump_shift, flow)
+            # motor = self._net(__phi_z + np.sign(d_phi) * self.bump_shift, flow)
             # print "D_phi: % 2.2f" % np.rad2deg(motor),
             if isinstance(sun, np.ndarray) and sun.size == 8:
                 sun = decode_sun(sun)[0]
@@ -241,7 +241,7 @@ class CXAgent(Agent):
             self.hist["flow0"].append(flow)
             self.hist["v0"].append(v)
             self.hist["v1"].append(v_trans)
-            self.hist["theta"].append(sun)
+            self.hist["theta_z"].append(sun)
             self.hist["tl2"].append(self._net.tl2)
             self.hist["cl1"].append(self._net.cl1)
             self.hist["cpu1"].append(self._net.cpu1)
@@ -422,7 +422,7 @@ if __name__ == "__main__":
 
             plt.subplot(5, 2, 3)
             plt.grid()
-            plt.plot(x, np.array(agent.hist["theta"]), label="theta")
+            plt.plot(x, np.array(agent.hist["theta_z"]), label="theta_z")
             v0 = np.arctan2(-np.array(agent.hist["v0"])[:, 1], np.array(agent.hist["v0"])[:, 0])
             v0 = (v0 + np.pi) % (2 * np.pi) - np.pi
             v1 = np.arctan2(-np.array(agent.hist["v1"])[:, 1], np.array(agent.hist["v1"])[:, 0])
@@ -434,7 +434,7 @@ if __name__ == "__main__":
             plt.xlim(xlim)
             plt.yticks([-np.pi, 0, np.pi], ["-pi", "0", "pi"])
             plt.ylim([-np.pi, np.pi])
-            plt.ylabel("__phi")
+            plt.ylabel("__phi_z")
 
             plt.subplot(5, 2, 5)
             plt.grid()

net/cx.py
@@ -559,10 +559,10 @@ if __name__ == "__main__":
         print "xy (reconstructed):", xy2.shape
         print "Is reconstruction accurate?", np.allclose(xy, xy2)
 
-        print "__phi:", phi.shape
+        print "__phi_z:", phi.shape
         x, y = xy[:, 0], xy[:, 1]
         phi2 = np.roll((np.arctan2(v[:, 0], -v[:, 1]) + np.pi) % (2 * np.pi) - np.pi, -1)
-        print "__phi (reconstructed):", phi2.shape
+        print "__phi_z (reconstructed):", phi2.shape
         print "Is reconstruction accurate?", np.allclose(phi[:-1], phi2[:-1])
 
         n_sensors = 200

world/geometry.py
@@ -573,7 +573,7 @@ class Route(object):
     def from_file(cls, filename):
         data = np.load(filename)
         new_route = Route(
-            xs=data['x'], ys=data['y'], zs=data['z'], phis=data['phi'], condition=Hybrid.from_array(data['condition']),
+            xs=data['x'], ys=data['y'], zs=data['z'], phis=data['phi_z'], condition=Hybrid.from_array(data['condition']),
             agent_no=data['agent'], route_no=data['route'])
 
         return new_route  # type: Route
@@ -589,7 +589,7 @@ def route_like(r, xs=None, ys=None, zs=None, phis=None,
     if zs is not None:
         new_route.z = np.array(zs)
     if phis is not None:
-        new_route.phi = np.array(phis)
+        new_route.phi_z = np.array(phis)
     if condition is not None:
         new_route.condition = condition
     if agent_no is not None: